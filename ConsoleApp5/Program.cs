73b1a62524b506b53ebc9b4a424cb81c88ba170f
Мария Журинова <Zhurinova_2003@mail.ru>
2022-03-29 17:47:11 +0300

    fghj

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -14,8 +14,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main()
         {
             Console.WriteLine($"Для {Darkland.регистрация} нажмите {(int)Darkland.регистрация}") ;
-    
-            Console.WriteLine($"Для {Darkland.бой} нажмите {(int)Darkland.бой}");
             Console.WriteLine($"Для {Darkland.выход}а нажмите {(int)Darkland.выход}");
         }
     }
